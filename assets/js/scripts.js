c39f0982ed92c43e5ae23ca056af5f51352a87af
saikat007hasan <saikathasan441@gmail.com>
2022-11-29 19:22:38 +0600

    testimonial

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -26,26 +26,44 @@ $(document).ready(function (){
               slidesToScroll: 1
             }
           },
+
+          {
+            breakpoint: 765,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          },
+
           {
             breakpoint: 600,
             settings: {
               slidesToShow: 1,
               slidesToScroll: 1
             }
+          },
+          {
+            breakpoint: 380,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
           }
+
         ]          
        });
+
     $('.testimonial-list').slick({
         autoplay: false,
         infinite: true,
-        slidesToShow: 3,
+        slidesToShow: 4,
         slidesToScroll: 3,
          dots: true,
         responsive: [
           {
             breakpoint: 1024,
             settings: {
-              slidesToShow: 2,
+              slidesToShow: 3,
               slidesToScroll: 2,
               infinite: true,
               dots: true
@@ -54,7 +72,7 @@ $(document).ready(function (){
           {
             breakpoint: 600,
             settings: {
-              slidesToShow: 1,
+              slidesToShow: 2,
               slidesToScroll: 1
             }
           },
