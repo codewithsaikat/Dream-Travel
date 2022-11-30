2caea8d85b2f504ccf61bc663a7e80825804be3f
saikat007hasan <saikathasan441@gmail.com>
2022-11-30 23:42:01 +0600

    ddd

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -53,39 +53,39 @@ $(document).ready(function (){
         ]          
        });
 
-    $('.testimonial-list').slick({
-        autoplay: false,
-        infinite: true,  
-        slidesToShow: 4,
-        slidesToScroll: 3,
-         dots: true,
-        responsive: [
-          {
-            breakpoint: 1024,
-            settings: {
-              slidesToShow: 2,
-              slidesToScroll: 2,
-              infinite: true,
-              dots: true
-            }
-          },
-          {
-            breakpoint: 600,
-            settings: {
-              slidesToShow: 2,
-              slidesToScroll: 1
-            }
-          },
-          {
-            breakpoint: 480,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1
-            }
-          }
-        ]
+    // $('.testimonial-list').slick({
+    //     autoplay: false,
+    //     infinite: true,  
+    //     slidesToShow: 4,
+    //     slidesToScroll: 3,
+    //      dots: true,
+    //     responsive: [
+    //       {
+    //         breakpoint: 1024,
+    //         settings: {
+    //           slidesToShow: 2,
+    //           slidesToScroll: 2,
+    //           infinite: true,
+    //           dots: true
+    //         }
+    //       },
+    //       {
+    //         breakpoint: 600,
+    //         settings: {
+    //           slidesToShow: 2,
+    //           slidesToScroll: 1
+    //         }
+    //       },
+    //       {
+    //         breakpoint: 480,
+    //         settings: {
+    //           slidesToShow: 1,
+    //           slidesToScroll: 1
+    //         }
+    //       }
+    //     ]
     
-       })
+    //    })
 
 });
 
