645669f4924997c65f654bbd38a34d4183b04a4a
saikat007hasan <saikathasan441@gmail.com>
2022-12-05 07:34:14 +0600

    testi

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -51,41 +51,45 @@ $(document).ready(function (){
           }
 
         ]          
-       });
+       });   
 
-    $('.testimonial-list').slick({
-        // autoplay: true,
-        infinite: true,  
-        slidesToShow: 4,
-        slidesToScroll: 3,
-         dots: false,
-        responsive: [
-          {
-            breakpoint: 1024,
-            settings: {
-              slidesToShow: 2,
-              slidesToScroll: 2,
-              infinite: true,
-              dots: true
-            }
-          },
-          {
-            breakpoint: 600,
-            settings: {
-              slidesToShow: 2,
-              slidesToScroll: 1
-            }
-          },
-          {
-            breakpoint: 480,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1
-            }
-          }
-        ]
-    
-       })
+});
+
+$(document).ready(function (){
+
+  $('.testimonial-list').slick({
+    // autoplay: true,
+    infinite: true,  
+    slidesToShow: 4,
+    slidesToScroll: 3,
+     dots: false,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+
+  })
 
 });
 
